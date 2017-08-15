23cf6cc9790c72308ca07e0b03b83d7e32ab357a
Mingriweiji-github <2822072760@qq.com>
2017-08-15 14:18:37 +0800

    main vc

---
+++ Tang/Classes/Main/Controller/KKBaseViewController.swift
@@ -0,0 +1,31 @@
+//
+//  KKBaseViewController.swift
+//  Tang
+//
+//  Created by M_Li on 2017/8/15.
+//  Copyright © 2017年 M_Li. All rights reserved.
+//
+
+import UIKit
+
+class KKBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+    
+        return .lightContent
+    }
+
+    
+}
