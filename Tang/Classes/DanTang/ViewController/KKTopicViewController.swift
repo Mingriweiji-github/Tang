09ad038da19e8b2351ecf4ade96842a6f8a310b7
Mingriweiji-github <2822072760@qq.com>
2017-08-16 21:57:41 +0800

    KKTopicViewController.swift supplement

--- Tang/Classes/DanTang/ViewController/KKTopicViewController.swift
+++ Tang/Classes/DanTang/ViewController/KKTopicViewController.swift
@@ -21,11 +21,11 @@ class KKTopicViewController: UITableViewController {
         weak var weakSelf = self
         KKNetworkTool.sharedInstance.requestHomeData(id: type) { (homeItmes) in
             weakSelf!.itmes = homeItmes
-            
-            
+            weakSelf?.tableView.reloadData()
         }
-        
 
+        refreshControl?.addTarget(self, action: #selector(reloadHomeData), for: .valueChanged)
+        
     }
 
     func setupTableView(){
@@ -35,14 +35,36 @@ class KKTopicViewController: UITableViewController {
         
         tableView.register(UINib(nibName:String(describing: KKHomeCell.self), bundle: nil), forCellReuseIdentifier: "KKHomeCell")
     }
+    func reloadHomeData() {
     
+       weak var weakSelf = self
+        KKNetworkTool.sharedInstance.requestHomeData(id: type) { (homeItems) in
+            
+            weakSelf?.itmes = homeItems
+            weakSelf?.tableView.reloadData()
+            weakSelf?.refreshControl?.endRefreshing()
+        }
+        
+    }
+    //MARK: UITableViewDataSource
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return itmes.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let homeCell = tableView.dequeueReusableCell(withIdentifier: homeCellIdentifier, for: indexPath) as! KKHomeCell
         
+        homeCell.homeItem = itmes[indexPath.row]
+        
+        homeCell.delegate = self as! KKHomeCellDelegate
+        
         return homeCell
     }
+
+    //KKHomeCellDelegate
+    func homeCellDidClickedFavouriteButton(button:UIButton){
+    
+        print("代理方式实现了")
+    }
+    
     
 }
