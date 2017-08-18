d87de08b96930645edfe06ec80c88b8222050f1b
Mingriweiji-github <2822072760@qq.com>
2017-08-18 18:21:11 +0800

    单品详情footer

---
+++ Tang/Classes/Product/View/KKCommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  KKCommentCell.swift
+//  Tang
+//
+//  Created by M_Li on 2017/8/18.
+//  Copyright © 2017年 M_Li. All rights reserved.
+//
+
+import UIKit
+
+class KKCommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
